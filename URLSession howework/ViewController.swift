fba5e0542129c9e4c592fdbb42cdf91aa6b60dd6
Vasiiy <nikolaev.vasiliy.96@mail.ru>
2020-01-15 18:31:24 +0500

    Initial Commit

---
+++ URLSession howework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSession howework
+//
+//  Created by Васлий Николаев on 15.01.2020.
+//  Copyright © 2020 Васлий Николаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
